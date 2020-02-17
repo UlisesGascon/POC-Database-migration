c1a0fc955fceeabecd156423ee1ccd348a57021a
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-02-17 17:38:23 +0100

    Added config

---
+++ data-migration/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    db: {
+        mongo: {
+            uri: "mongodb://localhost",
+            databaseName: "quotes",
+            collection: "quotes"
+        },
+        pg: {
+            host: "localhost",
+            port: 5432,
+            password: 'password',
+            database: 'postgres',
+            user: 'postgres',
+            max: 10,
+            idleTimeoutMillis: 30000,
+            sql: 'sql/queries',
+            ssl: false
+        }
+    },
+    seeds: {
+        total: 1000
+    }
+}
